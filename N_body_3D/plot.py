8d9a2dc6e69a9eb2e2b0f07a8c16da32b91d453f
WoutMommen <57433859+WoutMommen@users.noreply.github.com>
2019-11-20 08:54:09 +0100

    Add files via upload

--- N_body_3D/plot.py
+++ N_body_3D/plot.py
@@ -1,10 +1,15 @@
 import math as m
 import numpy as np
 import scipy.special
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-from matplotlib import animation
+#from matplotlib import animation
 from mpl_toolkits.mplot3d import Axes3D
 import csv
+#from moviepy.editor import VideoClip
+from matplotlib.animation import FFMpegWriter
+
 
 #Loading in data
 p, x, y, z = np.genfromtxt("test.txt", unpack = True)
@@ -12,7 +17,8 @@ p, x, y, z = np.genfromtxt("test.txt", unpack = True)
 #print(x)
 #print(y)
 N = 3
-iterations = 100000;
+print_step = 10
+iterations = int(7000/print_step);
 x1 = np.zeros((iterations))
 y1 = np.zeros((iterations))
 x2 = np.zeros((iterations))
@@ -56,16 +62,69 @@ ymax = 4.5
 zmin = -4.5
 zmax = 4.5
 
+'''
 fig = plt.figure()
 ax = Axes3D(fig)
-ax.plot(x1,y1,z1)
-ax.plot(x2,y2,z2)
-ax.plot(x3,y3,z3)
-#plt.grid()
-#plt.legend()
+ax.plot(x1,y1,z1, label='Deeltje 1')
+ax.plot(x2,y2,z2, label = 'Deeltje 2')
+ax.plot(x3,y3,z3, label = 'Deeltje 3')
+plt.grid()
+plt.legend()
 axes = plt.gca()
-axes.set_xlim([xmin,xmax])
-axes.set_ylim([ymin,ymax])
-axes.set_zlim([zmin,zmax])
+#axes.set_xlim([xmin,xmax])
+#axes.set_ylim([ymin,ymax])
+#axes.set_zlim([zmin,zmax])
+#plt.plot(x1,y1)
+#plt.plot(x2,y2)
+#plt.plot(x3,y3)
 plt.show()
+'''
+#Animation
+'''
+def make_frame(t):
+    """ returns an image of the frame at time t """
+    # ... create the frame with any library
+    return frame_for_time_t # (Height x Width x 3) Numpy array
 
+animation = VideoClip(make_frame, duration=3) # 3-second clip
+
+# For the export, many options/formats/optimizations are supported
+animation.write_videofile("my_animation.mp4", fps=24) # export as video
+animation.write_gif("my_animation.gif", fps=24) # export as GIF (slow)
+t = 0.
+h = 0.001
+'''
+
+metadata = dict(title='Movie Test', artist='Matplotlib',
+                comment='Movie support!')
+writer = FFMpegWriter(fps=60, metadata=metadata)
+
+fig = plt.figure()
+t, = plt.plot([], [], linewidth = 1)
+l, = plt.plot([], [], 'k-o')
+k, = plt.plot([], [], 'k-o')
+s, = plt.plot([], [], 'k-o')
+
+plt.xlim(-1.5, 1.5)
+plt.ylim(-1.5, 1.5)
+
+with writer.saving(fig, "writer_test.mp4", 100):
+    for i in range(iterations):
+        print(i)
+        t.set_data(x3, y3)
+        l.set_data(x1[i], y1[i])
+        k.set_data(x2[i], y2[i])
+        s.set_data(x3[i], y3[i])
+        writer.grab_frame()
+'''
+for i in range(2):
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(x1[i],y1[i],z1[i], label='Particle 1')
+    ax.scatter(x2[i],y2[i],z2[i], label = 'Particle 2')
+    ax.scatter(x3[i],y3[i],z3[i], label = 'Particle 3')
+    plt.grid()
+    plt.legend()
+    make_frame(t)
+    t += h
+'''
