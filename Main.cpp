2f0906c53a0bba940fd8d823456893901f7a7e06
Lukas De Greve <lukas.degreve@hotmail.com>
2019-12-06 10:41:16 +0100

    update

--- Main.cpp
+++ Main.cpp
@@ -8,22 +8,27 @@
 #include <cstddef>
 #include <chrono>
 #include <tuple>
-#include "Vector.cpp"
 #include "tree.cpp"
+#include "ezETAProgressBar.hpp"
 using namespace std;
 
 int main() { 
 vector<Vec> positions;
 vector<Vec> velocities;
-int i = 0;
+int i = 0;              // for loop variables
 int j = 0;
-int integration_time_steps = 25000;
-double time_step = 0.0001;
+int integration_time_steps = 250;
+double time_step = 0.0005;
+int N;                  // number of particles in the simulation
+double delta = 0.8;     // Tree clustering variable
+double upsilon = 0.5;       // softening parameter
+
+ez::ezETAProgressBar eta_time(integration_time_steps);
 
 chrono::steady_clock::time_point begin = chrono::steady_clock::now();
 
 ifstream inFile;
-inFile.open("/home/lukasdegreve/Documents/School/Astrofysische simulaties/Astrofysische_project/initial conditions/initial_conditions_100particles.txt",std::ios::in);
+inFile.open("/home/lukasdegreve/Documents/School/Astrofysische simulaties/Astrofysische_project/initial conditions/initial_conditions_collide_100particles.txt",std::ios::in);
 double posx,posy,posz,velx,vely,velz;
 double num = 0.;
 
@@ -38,15 +43,23 @@ while(inFile >> posx >> posy >> posz >> velx >> vely >> velz)
     positions.push_back(Vec(posx,posy,posz));
     velocities.push_back(Vec(velx,vely,velz));
 }
-for (int i = 0; i <= integration_time_steps; i++)
+N = positions.size();
+
+// output file for plots
+ofstream outfile("Tree_data.txt");
+outfile << setprecision(8);
+
+eta_time.start();
+for (int i = 0; i <= integration_time_steps; i++, ++eta_time)
 {
     // create tree and return the root node
     node* tree = maketree(positions, num);
-    if (i % 100 == 0){cout << i << endl;}
+    // if (i % 100 == 0){cout << i << endl;}
 
-    for (int j = 0; j < positions.size(); j++)
+    //for (int j = 0; j < positions.size(); j++)
+
     {
-        Vec acc = force(tree, positions[j]);
+        Vec acc = force(tree, positions[j], N, delta, upsilon);
 
         // A different integration scheme is needed for the first time step in comparison to the rest and the last
         if (i == 0)
@@ -63,16 +76,24 @@ for (int i = 0; i <= integration_time_steps; i++)
         {
             velocities[j] += 0.5 * acc * time_step;
             // When calculating energy, put an if statement here and print the positions and velocities
+
             velocities[j] += 0.5 * acc * time_step;
-            // putting the if statement here gives you the velocities at a different time step than the positions
+            // putting the if statement here gives you the velocities at a different time step than the positions --> DONT DO THIS
             positions[j] += time_step * velocities[j];
         }
+        if (i % 10 == 0)
+        {
+            outfile << j << ' ' << positions[j].out() << endl;
+            if (j == positions.size() - 1) {outfile << endl;}
+
+        }
+
     }
 
 }
-cout << "position 1" << positions[0].out() << endl;
 chrono::steady_clock::time_point end = chrono::steady_clock::now();
 cout << "Time difference = " << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << "ms" << endl;
 
 return 0; 
+
 }
