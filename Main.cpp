63f1be89be13c8da647afb7e2b1486672a56ddb4
Lukas De Greve <lukas.degreve@hotmail.com>
2019-12-09 20:27:26 +0100

    energy

--- Main.cpp
+++ Main.cpp
@@ -17,11 +17,21 @@ vector<Vec> positions;
 vector<Vec> velocities;
 int i = 0;              // for loop variables
 int j = 0;
+int k = 0;
+int l = 0;
 int integration_time_steps = 250;
 double time_step = 0.0005;
 int N;                  // number of particles in the simulation
 double delta = 0.8;     // Tree clustering variable
-double upsilon = 0.5;       // softening parameter
+double epsilon = 0.5;       // softening parameter
+vector<Vec> E_pos_list;
+vector<Vec> E_vel_list;
+double E_kin;
+double E_pot;
+double E_tot;
+double E_rel;
+Vec diff;
+
 
 ez::ezETAProgressBar eta_time(integration_time_steps);
 
@@ -45,50 +55,80 @@ while(inFile >> posx >> posy >> posz >> velx >> vely >> velz)
 }
 N = positions.size();
 
-// output file for plots
+// output file for plots and energy
 ofstream outfile("Tree_data.txt");
 outfile << setprecision(8);
+ofstream outfile2("energy.txt");
+outfile2 << setprecision(8);
 
 eta_time.start();
 for (int i = 0; i <= integration_time_steps; i++, ++eta_time)
 {
     // create tree and return the root node
     node* tree = maketree(positions, num);
-    // if (i % 100 == 0){cout << i << endl;}
+    E_vel_list.clear();
+    E_pos_list.clear();
 
-    //for (int j = 0; j < positions.size(); j++)
-
+    for (j = 0; j < N; j++)
     {
-        Vec acc = force(tree, positions[j], N, delta, upsilon);
-
+        Vec acc = force(tree, positions[j], N, delta, epsilon);
         // A different integration scheme is needed for the first time step in comparison to the rest and the last
         if (i == 0)
         {
             velocities[j] += 0.5 * acc * time_step;
             positions[j] += time_step * velocities[j];
+            E_pos_list.push_back(positions[j]);
+            E_vel_list.push_back(velocities[j]);
         }
 
         else if (i == integration_time_steps)
         {
             velocities[j] += 0.5 * acc * time_step;
+            E_pos_list.push_back(positions[j]);
+            E_vel_list.push_back(velocities[j]);
         }
         else
         {
             velocities[j] += 0.5 * acc * time_step;
-            // When calculating energy, put an if statement here and print the positions and velocities
+            // When calculating energy, calculate it here and not in the next step
+            E_pos_list.push_back(positions[j]);
+            E_vel_list.push_back(velocities[j]);
 
             velocities[j] += 0.5 * acc * time_step;
-            // putting the if statement here gives you the velocities at a different time step than the positions --> DONT DO THIS
             positions[j] += time_step * velocities[j];
         }
         if (i % 10 == 0)
         {
             outfile << j << ' ' << positions[j].out() << endl;
-            if (j == positions.size() - 1) {outfile << endl;}
+            if (j == N - 1) {outfile << endl;}
 
         }
 
     }
+    
+    // loops for calculating the energy
+    E_kin = 0;
+    E_pot = 0;
+    for (k=0; k != N; k++)
+    {
+        E_kin += E_vel_list[k].norm2();
+
+        for (l=0; l!=N; l++)
+        {  
+            if ( k != l)
+            {
+                diff = E_pos_list[k] - E_pos_list[l];
+                E_pot += 1/(diff.norm());
+            }
+            
+        }
+    }
+    E_kin /= 2 * N;
+    E_pot /= -2 * N * N;
+    E_tot = E_pot + E_kin;
+    E_rel = (E_tot + 0.25) / (-0.25) * 100;
+    outfile2 << E_rel << " " << E_tot << " " << E_pot << " " << E_kin << endl;
+
 
 }
 chrono::steady_clock::time_point end = chrono::steady_clock::now();
--- initial conditions/initial_conditions.cpp
+++ initial conditions/initial_conditions.cpp
@@ -15,148 +15,148 @@ using namespace std;
 
 int main()
 {   
-    // declare variables
-    Vec pos;
-    Vec avg_pos;
-    Vec vel;
-    Vec avg_vel;
-    Vec diff;
-    vector<Vec> pos_list;
-    vector<Vec> vel_list;
-    double E_kin;
-    double E_pot;
-    int particles; 
-    string filename;
-    mt19937_64 rng;
+// declare variables
+Vec pos;
+Vec avg_pos;
+Vec vel;
+Vec avg_vel;
+Vec diff;
+vector<Vec> pos_list;
+vector<Vec> vel_list;
+double E_kin;
+double E_pot;
+int particles; 
+string filename;
+mt19937_64 rng;
 
-    // aantal deeltjes in de simulatie
-    cout << "How many particles do you want to generate" << endl;
-    cin >> particles;
+// aantal deeltjes in de simulatie
+cout << "How many particles do you want to generate" << endl;
+cin >> particles;
 
-    // check speed
-    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+// check speed
+chrono::steady_clock::time_point begin = chrono::steady_clock::now();
 
-    // sets file for output
-    filename = "initial_conditions_" + to_string(particles) + "particles.txt";
-    ofstream outfile (filename);
-    outfile << setprecision(8);
+// sets file for output
+filename = "initial_conditions_" + to_string(particles) + "particles.txt";
+ofstream outfile (filename);
+outfile << setprecision(8);
 
-    // initialize the random number generator with time-dependent seed
-    uint64_t timeSeed = chrono::high_resolution_clock::now().time_since_epoch().count();
-    seed_seq ss{uint32_t(timeSeed & 0xffffffff), uint32_t(timeSeed>>32)};
-    rng.seed(ss);
-    // initialize a uniform distribution between 0 and 1
-    uniform_real_distribution<double> unif(0.0, 1.0);
-    auto random_number  = bind(unif, rng);
+// initialize the random number generator with time-dependent seed
+uint64_t timeSeed = chrono::high_resolution_clock::now().time_since_epoch().count();
+seed_seq ss{uint32_t(timeSeed & 0xffffffff), uint32_t(timeSeed>>32)};
+rng.seed(ss);
+// initialize a uniform distribution between 0 and 1
+uniform_real_distribution<double> unif(0.0, 1.0);
+auto random_number  = bind(unif, rng);
 
 
-    for (int i = 0; i < particles ; i++)
+for (int i = 0; i < particles ; i++)
+{
+    // positions according to a plumber model
+    double rand1 = random_number();
+    double rand2 = random_number();
+    double rand3 = random_number();
+
+    double theta = acos(1 - 2 * rand1);
+    double phi = 2 * M_PI * rand2;
+    double radius = 3 * M_PI / (16 * sqrt(pow(rand3, -2.0/3.0) - 1.0));
+    
+    double x = radius * sin(theta) * cos(phi);
+    double y = radius * sin(theta) * sin(phi);
+    double z = radius * cos(theta);
+
+    Vec pos(x, y, z);
+    avg_pos += pos;
+    pos_list.push_back(pos);
+
+    // velocities according to an isotropic plumber model
+    bool not_generated = true;
+
+    do
     {
-        // positions according to a plumber model
-        double rand1 = random_number();
-        double rand2 = random_number();
-        double rand3 = random_number();
+        double rand4 = random_number();           // q
+        double rand5 = random_number();
 
-        double theta = acos(1 - 2 * rand1);
-        double phi = 2 * M_PI * rand2;
-        double radius = 3 * M_PI / (16 * sqrt(pow(rand3, -2.0/3.0) - 1.0));
-        
-        double x = radius * sin(theta) * cos(phi);
-        double y = radius * sin(theta) * sin(phi);
-        double z = radius * cos(theta);
+        double p_max = 50176.0 * sqrt(7) / (19683.0 * M_PI) * rand5;
+        double p = 512 / (7 * M_PI) * pow(1 - rand4, 7.0/2.0) * rand4 * rand4;
 
-        Vec pos(x, y, z);
-        avg_pos += pos;
-        pos_list.push_back(pos);
-
-        // velocities according to an isotropic plumber model
-        bool not_generated = true;
-
-        do
+        if ( p_max < p )
         {
-            double rand4 = random_number();           // q
-            double rand5 = random_number();
+            not_generated = false;
+            double rand6 = random_number();
+            double rand7 = random_number();
 
-            double p_max = 50176.0 * sqrt(7) / (19683.0 * M_PI) * rand5;
-            double p = 512 / (7 * M_PI) * pow(1 - rand4, 7.0/2.0) * rand4 * rand4;
+            double theta = acos(1 - 2 * rand6);
+            double phi = 2 * M_PI * rand7;
 
-            if ( p_max < p )
-            {
-                not_generated = false;
-                double rand6 = random_number();
-                double rand7 = random_number();
+            double v_esc = sqrt(2 / sqrt(pos.norm2() + 9 * M_PI / 256));
+            double q_esc = v_esc * rand4;
 
-                double theta = acos(1 - 2 * rand6);
-                double phi = 2 * M_PI * rand7;
+            double v_x = q_esc * sin(theta) * cos(phi);
+            double v_y = q_esc * sin(theta) * sin(phi);
+            double v_z = q_esc * cos(theta);
 
-                double v_esc = sqrt(2 / sqrt(pos.norm2() + 9 * M_PI / 256));
-                double q_esc = v_esc * rand4;
-
-                double v_x = q_esc * sin(theta) * cos(phi);
-                double v_y = q_esc * sin(theta) * sin(phi);
-                double v_z = q_esc * cos(theta);
-
-                Vec vel(v_x, v_y, v_z);
-                avg_vel += vel;
-                vel_list.push_back(vel);
-            }
-
+            Vec vel(v_x, v_y, v_z);
+            avg_vel += vel;
+            vel_list.push_back(vel);
         }
-        while ( not_generated );
+
     }
+    while ( not_generated );
+}
 
-    // COM-position and velocity
-    avg_vel /= particles;
-    avg_pos /= particles;
+// COM-position and velocity
+avg_vel /= particles;
+avg_pos /= particles;
 
-    // centering: we read the previous file and subtract the COM-velocity and position from every point
-   for (size_t i=0; i!=pos_list.size(); i++)
+// centering: we read the list and subtract the COM-velocity and position from every point
+for (size_t i=0; i!=pos_list.size(); i++)
+{
+    pos_list[i] -= avg_pos;
+    vel_list[i] -= avg_vel;
+}   
+
+// scaling: we rescale the coordinates so that the virial theorem is true
+// the total potential energy must be -1/2 and the total kinetic energy 1/4 
+for (size_t i=0; i!=pos_list.size(); i++)
+{
+    E_kin += vel_list[i].norm2();
+    if (i % 100 == 0)
     {
-       pos_list[i] -= avg_pos;
-       vel_list[i] -= avg_vel;
-    }   
+        cout << i << endl;
+    }
     
-    // scaling: we rescale the coordinates so that the virial theorem is true
-    // the total potential energy must be -1/2 and the total kinetic energy 1/4 
-    for (size_t i=0; i!=pos_list.size(); i++)
-    {
-        E_kin += vel_list[i].norm2();
-        if (i % 100 == 0)
+
+    for (size_t j=0; j!=pos_list.size(); j++)
+    {  
+        if ( i != j)
         {
-            cout << i << endl;
+            diff = pos_list[i] - pos_list[j];
+            E_pot += 1/(diff.norm());
         }
         
-
-        for (size_t j=0; j!=pos_list.size(); j++)
-        {  
-            if ( i != j)
-            {
-                diff = pos_list[i] - pos_list[j];
-                E_pot += 1/(diff.norm());
-            }
-            
-        }
     }
-    
-    E_kin /= 2 * particles;
-    E_pot /= 2 * particles * particles;
+}
 
-    for (size_t i=0; i!=pos_list.size(); i++)
-    {
-       pos_list[i] *= -1*2*E_pot;
-       vel_list[i] /= 2*sqrt(E_kin);
-    }
+E_kin /= 2 * particles;
+E_pot /= 2 * particles * particles;
 
-    
-    for (size_t i=0; i!=pos_list.size(); i++)
-    {
-        pos = pos_list[i];
-        vel = vel_list[i];
-        outfile << pos.out() << " " << vel.out() << endl;
-    }
-    // more speed checks
-    chrono::steady_clock::time_point end = chrono::steady_clock::now();
-    cout << "Time difference = " << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << "ms" << endl;
+for (size_t i=0; i!=pos_list.size(); i++)
+{
+    pos_list[i] *= -1*2*E_pot;
+    vel_list[i] /= 2*sqrt(E_kin);
+}
 
-    return (0);
+
+for (size_t i=0; i!=pos_list.size(); i++)
+{
+    pos = pos_list[i];
+    vel = vel_list[i];
+    outfile << pos.out() << " " << vel.out() << endl;
+}
+// more speed checks
+chrono::steady_clock::time_point end = chrono::steady_clock::now();
+cout << "Time difference = " << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << "ms" << endl;
+
+return (0);
 }
